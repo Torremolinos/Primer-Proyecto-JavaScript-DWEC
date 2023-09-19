b40510bc2512f3875d567cd450cf82a30cf366b7
Torremolinos <adrian_2054@hotmail.com>
2023-09-20 00:45:25 +0200

    Actualizacion del proyecto

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,18 @@
  // Función para generar un color aleatorio en formato hexadecimal
 function generarColorAleatorio() {
    const letras = "0123456789ABCDEF";
-   let color = "#";
+   var color = "#";
    for (let i = 0; i < 6; i++) {
        color += letras[Math.floor(Math.random() * 16)];
    }
    return color;
-   console.log(color);
 }
 
 // Función para cambiar el color de fondo de la página
 function cambiarColorDeFondo() {
    const colorAleatorio = generarColorAleatorio();
    document.body.style.backgroundColor = colorAleatorio;
+   console.log(`Color generado:${colorAleatorio}`);
 }
  // Asignar la función al botón
  const botonCambiarColor = document.getElementById("cambiarColor");
