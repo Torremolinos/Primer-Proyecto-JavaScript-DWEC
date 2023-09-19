22926e966d6a52c43381634216d7fabf77ff88e7
Torremolinos <adrian_2054@hotmail.com>
2023-09-19 23:53:28 +0200

    Actualizando mi proyecto

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,19 @@
-const btn = document.querySelector('button');
-const body = document.querySelector('body');
-
-const colors = ['blue', 'yellow', 'green', 'black', 'white'];
-
-btn.addEventListener('click', background);
-
-function background() {
-    var background = parseInt(Math.random() * colors.length);
-    body.style.backgroundColor = background(indice)
+ // Función para generar un color aleatorio en formato hexadecimal
+function generarColorAleatorio() {
+   const letras = "0123456789ABCDEF";
+   let color = "#";
+   for (let i = 0; i < 6; i++) {
+       color += letras[Math.floor(Math.random() * 16)];
+   }
+   return color;
+   console.log(color);
 }
+
+// Función para cambiar el color de fondo de la página
+function cambiarColorDeFondo() {
+   const colorAleatorio = generarColorAleatorio();
+   document.body.style.backgroundColor = colorAleatorio;
+}
+ // Asignar la función al botón
+ const botonCambiarColor = document.getElementById("cambiarColor");
+ botonCambiarColor.addEventListener("click", cambiarColorDeFondo);
