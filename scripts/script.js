25db9e4cde9f5fcda0255e7324c0ac0334228c7b
Torremolinos <adrian_2054@hotmail.com>
2023-09-20 09:54:36 +0200

    Añado un favicon al proyecto.

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,19 @@
- // Función para generar un color aleatorio en formato hexadecimal
-function generarColorAleatorio() {
+// Función para generar un color aleatorio en formato hexadecimal
+const generarColorAleatorio = () => {
    const letras = "0123456789ABCDEF";
    var color = "#";
    for (let i = 0; i < 6; i++) {
-       color += letras[Math.floor(Math.random() * 16)];
+      color += letras[Math.floor(Math.random() * 16)];
    }
    return color;
 }
 
 // Función para cambiar el color de fondo de la página
-function cambiarColorDeFondo() {
+const cambiarColorDeFondo = () => {
    const colorAleatorio = generarColorAleatorio();
    document.body.style.backgroundColor = colorAleatorio;
    console.log(`Color generado:${colorAleatorio}`);
 }
- // Asignar la función al botón
- const botonCambiarColor = document.getElementById("cambiarColor");
- botonCambiarColor.addEventListener("click", cambiarColorDeFondo);
+// Asignar la función al botón
+const botonCambiarColor = document.getElementById("cambiarColor");
+botonCambiarColor.addEventListener("click", cambiarColorDeFondo);
